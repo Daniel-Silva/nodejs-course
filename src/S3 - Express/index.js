082e2e19ca1fe43778cdcdf7d5660c548e08b17e
Daniel Silva <dani3l.dxp@gmail.com>
2020-02-01 09:47:24 -0300

    feat: entendendo e criando rotas

--- src/S3 - Express/index.js
+++ src/S3 - Express/index.js
@@ -1,5 +1,13 @@
 const express = require('express')();
 
+express.get('/', (request, response) => {
+	response.send('<html><body>PAGIA INICIAL</body></html>');
+});
+
+express.get('/blog', (request, response) => {
+	response.send('BLOG');
+});
+
 express.listen(3000, (error) => {
 	if (error) {
 		console.log(error);
